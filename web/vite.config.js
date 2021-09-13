0fa0a8f4f8502ba4f675931c37c33c17d16ab7ea
lidppp <1512282028@qq.com>
2021-09-13 23:39:58 +0800

    set: base config set: ignore file and dir add: sass, @types/node

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const path = require("path")
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+        port: 8888,
+        open: true
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src")
+        }
+    }
 })
